f20bed80e03fb66dd6bf1ecc00d03374fbfda607
rise0chen <rise0chen@163.com>
2022-02-21 15:38:22 +0800

    atomic

--- clock_source/src/std.rs
+++ clock_source/src/std.rs
@@ -1,11 +1,13 @@
 extern crate std;
+use std::sync::Once;
 use std::time::Instant;
 
 static mut TIME: Option<Instant> = None;
+static INIT: Once = Once::new();
 
 pub const CLOCK: fn() -> u64 = || unsafe {
-    if TIME.is_none() {
-        TIME = Some(Instant::now())
-    }
+    INIT.call_once(|| {
+        TIME = Some(Instant::now());
+    });
     TIME.unwrap().elapsed().as_nanos() as u64
 };
